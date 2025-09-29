2b33aab00f5b670e58e77a59e73571426b42022b
tan <13320385140@163.com>
2025-09-29 23:05:18 +0800

    1

--- api/db/init_mysql.sql
+++ api/db/init_mysql.sql
@@ -5,7 +5,7 @@ INSERT INTO `nav_category` (`ID`, `CATEGORY_NAME`, `SORT`) VALUES (4, '好用工
 INSERT INTO `nav_category` (`ID`, `CATEGORY_NAME`, `SORT`) VALUES (5, '文章推荐', 0);
 
 
-INSERT INTO `backup_config` (`ID`, `BACKUP_COUNT`, `DESCRIPTION`, `DRIVER_CLASS_NAME`, `ENABLED`, `LAST_BACKUP_TIME`, `PASSWORD`, `SCHEDULE`, `URL`, `USERNAME`) VALUES (1, 0, '测试', 'com.mysql.cj.jdbc.Driver', 1, NULL, 'root', '0 0 2 * * ?', 'jdbc:mysql://localhost:3306/test', 'root');
+INSERT INTO `backup_config` (`ID`, `BACKUP_COUNT`, `DESCRIPTION`, `DRIVER_CLASS_NAME`, `ENABLED`, `LAST_BACKUP_TIME`, `PASSWORD`, `SCHEDULE`, `URL`, `USERNAME`) VALUES (1, 0, '测试', 'com.mysql.cj.jdbc.Driver', 0, NULL, 'root', '0 0 2 * * ?', 'jdbc:mysql://localhost:3306/test', 'root');
 
 
 INSERT INTO `system_config` (`ID`, `CONFIG_KEY`, `DEFAULT_OPEN_MODE`, `HIDE_ADMIN_ENTRY`, `PASSWORD`, `SITE_LOGO`, `SITE_TITLE`, `USERNAME`) VALUES (1, 'MAIN', 1, 0, '123', NULL, 'Oasis', 'tan');
