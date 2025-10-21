c85950b8eed18b5009f16a9a111a67d3a3750ded
tan <13320385140@163.com>
2025-10-21 11:58:46 +0800

    123

--- api/db/init_mysql.sql
+++ api/db/init_mysql.sql
@@ -7,7 +7,7 @@ INSERT INTO NAV_CATEGORY(ID, CATEGORY_NAME, SORT) VALUES (4, '组件', 4);
 INSERT INTO NAV_CATEGORY(ID, CATEGORY_NAME, SORT) VALUES (5, '工具', 5);
 
 
-INSERT INTO `backup_config` (`ID`, `BACKUP_COUNT`, `DESCRIPTION`, `DRIVER_CLASS_NAME`, `ENABLED`, `LAST_BACKUP_TIME`, `PASSWORD`, `SCHEDULE`, `URL`, `USERNAME`) VALUES (1, 0, '测试', 'com.mysql.cj.jdbc.Driver', 0, NULL, 'root', '0 0 2 * * ?', 'jdbc:mysql://localhost:3306/oasis', 'root');
+INSERT INTO `backup_config` (`ID`, `BACKUP_COUNT`, `DESCRIPTION`, `DRIVER_CLASS_NAME`, `ENABLED`, `LAST_BACKUP_TIME`, `PASSWORD`, `SCHEDULE`, `URL`, `USERNAME`) VALUES (1, 0, '数据备份到MySQL', 'com.mysql.cj.jdbc.Driver', 0, NULL, 'root', '0 0 2 * * ?', 'jdbc:mysql://127.0.0.1:3306/oasis', 'root');
 
 
 INSERT INTO `system_config` (`ID`, `CONFIG_KEY`, `DEFAULT_OPEN_MODE`, `HIDE_ADMIN_ENTRY`, `PASSWORD`, `SITE_LOGO`, `SITE_TITLE`, `USERNAME`) VALUES (1, 'MAIN', 1, 0, '123', NULL, 'Oasis', 'tan');
