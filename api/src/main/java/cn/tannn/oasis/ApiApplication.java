eff902f3e16d7bd8e22153bef450c2ab882d4cd9
tan <13320385140@163.com>
2025-10-23 14:05:40 +0800

    log

--- api/src/main/java/cn/tannn/oasis/ApiApplication.java
+++ api/src/main/java/cn/tannn/oasis/ApiApplication.java
@@ -4,14 +4,25 @@ import cn.tannn.oasis.controller.DataBackupController;
 import cn.tannn.oasis.controller.LoginController;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static cn.tannn.jdevelops.knife4j.core.constant.PublicConstant.COLON;
+import static cn.tannn.jdevelops.knife4j.core.constant.PublicConstant.SPIRIT;
+import static cn.tannn.jdevelops.knife4j.core.util.SwaggerUtil.getRealIp;
+
 @Slf4j
 @SpringBootApplication
 public class ApiApplication  implements ApplicationRunner {
+
+    @Value("${server.port:8080}")
+    private int serverPort;
+
+    @Value("${server.servlet.context-path:/}")
+    private String serverName;
 
     @Autowired
     private DataBackupController dataBackupController;
@@ -21,12 +32,6 @@ public class ApiApplication  implements ApplicationRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
-        log.info(""" 
-                
-                =====================================================================
-                ========web地址：http://192.168.1.71:1249=============================
-                =====================================================================
-                """);
     }
 
     @Override
@@ -37,5 +42,17 @@ public class ApiApplication  implements ApplicationRunner {
 
         log.info("初始化数据");
         loginController.initSysConfig();
+
+
+        if (SPIRIT.equals(serverName)) {
+            serverName = "";
+        }
+
+        log.info(""" 
+                
+                =====================================================================
+                ========web地址：(http://%s =============================
+                =====================================================================
+                """.formatted(getRealIp() + COLON + serverPort + serverName ));
     }
 }
